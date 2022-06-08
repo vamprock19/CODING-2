1b81fe3555c76ff8ca500f75a1a89d96707cb44e
vamprock19 <94476879+vamprock19@users.noreply.github.com>
2022-06-08 18:55:16 -0400

    Add files via upload

---
+++ StaticMath/MathLibrary/MATHEMATICS.cpp
@@ -0,0 +1,24 @@
+#include"MATH.h"
+
+namespace mathlibrary
+{
+    double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
---
+++ StaticMath/MathLibrary/MATHS.h
@@ -0,0 +1,20 @@
+//MATH.h
+#pragma once
+
+
+namespace mathlibrary
+{
+	class Arithmetic
+	{
+	public:
+		//RETURNS A + B
+		static double Add(double a, double b);
+		//RETURNS A - B
+		static double Subtract(double a, double b);
+		// RETURNS A * B
+		static double Multiply(double a, double b);
+		// RETURNS A / B
+		static double Divide(double a, double b);
+	};
+
+}
